cec61f0b1e4075158ab77772da9f699ca75a5973
cytescale <nikhil@cytescale.com>
2021-06-17 03:22:42 +0530

    commit

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ class server_entry{
     router.post('/api/gitUpdate',async(req,res,next)=>{
       console.log("Git update pushed");
       let server_respon = null;
-      exec("cd /home/ubuntu/nexus/ && git stash && git pull && pm2 restart nexus", (error, stdout, stderr) => {
+      exec(`eval "$(ssh-agent -s)" && ssh-add ~/.ssh/gitkey && cd /home/ubuntu/nexus/ && git stash && git pull && pm2 restart nexus`, (error, stdout, stderr) => {
         if (error) {
           server_respon = error.message; 
           console.log(`error: ${error.message}`);
