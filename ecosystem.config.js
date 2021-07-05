e42ad351b123eab21acb268b4674bc565d655364
cytescale <nikhil@cytescale.com>
2021-07-05 10:29:42 +0530

    server update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,9 @@
 module.exports = {
   apps : [{
     name:'nexus',
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+    script: 'nodemon',
+    watch: true,
+    instances : "max",
+    exec_mode : "cluster"
+  }]
 };
